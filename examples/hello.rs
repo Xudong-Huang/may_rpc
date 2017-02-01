432c7b3d1dca5bf0d4195cbfce4f25c342928406
Xudong Huang <huangxu008@hotmail.com>
2017-02-01 20:28:31 +0800

    add rpc macro

---
+++ examples/hello.rs
@@ -0,0 +1,86 @@
+#[macro_use]
+extern crate corpc;
+#[macro_use]
+extern crate serde_derive;
+
+// cargo rustc --bin main -- -Z unstable-options --pretty expanded
+
+rpc! {
+    /// the connection type, default is Tcp
+    net: Udp;
+    /// Say hello
+    rpc hello(name: String) -> String;
+    /// add two number
+    rpc add(x: u32, y: u32 ) -> u32;
+}
+
+mod count {
+    rpc! {
+        /// get current count
+        rpc get_count() -> usize;
+    }
+}
+
+fn test_count() {
+    use std::sync::atomic::{AtomicUsize, Ordering};
+    use count;
+    struct CountImpl(AtomicUsize);
+    impl count::RpcSpec for CountImpl {
+        fn get_count(&self) -> usize {
+            self.0.fetch_add(1, Ordering::Relaxed)
+        }
+    }
+
+    let addr = ("127.0.0.1", 4000);
+    let server = count::RpcServer(CountImpl(AtomicUsize::new(0))).start(&addr).unwrap();
+    let mut client = count::RpcClient::connect(addr).unwrap();
+    client.set_timeout(::std::time::Duration::from_millis(100));
+
+    for _ in 0..10 {
+        let data = client.get_count();
+        println!("recv = {:?}", data);
+    }
+
+    unsafe { server.coroutine().cancel() };
+    server.join().ok();
+}
+
+fn test_hello() {
+    struct HelloImpl;
+    impl RpcSpec for HelloImpl {
+        fn hello(&self, name: String) -> String {
+            name
+        }
+
+        fn add(&self, x: u32, y: u32) -> u32 {
+            x + y
+        }
+    }
+
+    let addr = ("127.0.0.1", 4000);
+    let server = RpcServer(HelloImpl).start(&addr).unwrap();
+    let mut client = RpcClient::connect(addr).unwrap();
+    client.set_timeout(::std::time::Duration::from_millis(100));
+
+    for i in 0..10 {
+        let s = format!("Hello World! id={}", i);
+        let data = client.hello(s);
+        println!("recv = {:?}", data);
+    }
+
+    for i in 0..10 {
+        let data = client.add(i, i);
+        println!("recv = {:?}", data);
+    }
+
+    unsafe { server.coroutine().cancel() };
+    server.join().ok();
+}
+
+fn main() {
+    println!("test_hello");
+    test_hello();
+
+    println!("\n\ntest_hello");
+    test_count();
+}
--- src/lib.rs
+++ src/lib.rs
@@ -49,41 +49,230 @@
 //! ```
 //!
 
-#![deny(missing_docs)]
-// extern crate byteorder;
-// extern crate bytes;
-// #[macro_use]
-// extern crate log;
-// extern crate net2;
+// #![deny(missing_docs)]
 #[doc(hidden)]
-pub extern crate serde;
-#[macro_use]
-extern crate serde_derive;
-// extern crate take;
+pub extern crate conetty;
 
-// #[doc(hidden)]
-// pub extern crate bincode;
-// #[doc(hidden)]
-// pub extern crate serde;
-//
-// #[doc(hidden)]
-// pub use client::Client;
-pub use errors::Error;
-#[doc(hidden)]
-pub use errors::WireError;
+#[macro_export]
+macro_rules! rpc_client {
+    (Tcp) => {$crate::conetty::TcpClient};
+    (Udp) => {$crate::conetty::UdpClient};
+}
 
-/// Provides some utility error types, as well as a trait for spawning futures on the default event
-/// loop.
-// pub mod util;
-/// Provides the macro used for constructing rpc services and client stubs.
-// #[macro_use]
-// mod macros;
-/// Provides the base client stubs used by the service macro.
-// pub mod client;
-/// Provides the base server boilerplate used by service implementations.
-// pub mod server;
-/// Provides implementations of `ClientProto` and `ServerProto` that implement the tarpc protocol.
-/// The tarpc protocol is a length-delimited, bincode-serialized payload.
-// mod protocol;
-/// Provides a few different error types.
-mod errors;
+#[macro_export]
+macro_rules! rpc_server_start {
+    (Tcp, $me: ident, $addr: expr) => {$crate::conetty::TcpServer::start($me, $addr)};
+    (Udp, $me: ident, $addr: expr) => {$crate::conetty::UdpServer::start($me, $addr)};
+}
+
+
+/// The main macro that creates RPC services.
+///
+/// Rpc methods are specified, mirroring trait syntax:
+///
+/// ```
+/// # #[macro_use] extern crate corpc;
+/// # fn main() {}
+/// # rpc! {
+/// net: Tcp
+/// trait HelloRpc {
+///
+/// }
+/// /// Say hello
+/// rpc hello(name: String) -> String;
+/// # }
+/// ```
+///
+/// Attributes can be attached to each rpc. These attributes
+/// will then be attached to the generated service traits'
+/// corresponding `fn`s, as well as to the client stubs' RPCs.
+///
+/// The following items are expanded in the enclosing module:
+///
+/// * `RpcSpec`   -- the trait defining the RPC service that user need to impl
+/// * `RpcServer` -- the server that run the RPC service
+/// * `RpcClient` -- rpc client stubs implementation that wrap UdpClient/TcpClient
+///
+#[macro_export]
+macro_rules! rpc {
+// Entry point without net
+    (
+        $(
+            $(#[$attr:meta])*
+            rpc $fn_name:ident( $( $arg:ident : $in_:ty ),* ) $(-> $out:ty)*;
+        )*
+    ) => {
+        rpc! {
+            net: Tcp;
+            $(
+                $(#[$attr])*
+                rpc $fn_name( $( $arg : $in_ ),* ) $(-> $out)*;
+            )*
+        }
+    };
+// Entry point with net
+    (
+        $(#[$net_attr:meta])*
+        net: $net_type: ident;
+        $(
+            $(#[$attr:meta])*
+            rpc $fn_name:ident( $( $arg:ident : $in_:ty ),* ) $(-> $out:ty)*;
+        )*
+    ) => {
+        rpc! {
+            $(#[$net_attr])*
+            net: $net_type;
+            {
+                $(
+                    $(#[$attr])*
+                    rpc $fn_name( $( $arg : $in_ ),* ) $(-> $out)*;
+                )*
+            }
+        }
+    };
+// Pattern for when the next rpc has an implicit unit return type
+    (
+        $(#[$net_attr:meta])*
+        net: $net_type: ident;
+        {
+            $(#[$attr:meta])*
+            rpc $fn_name:ident( $( $arg:ident : $in_:ty ),* );
+
+            $( $unexpanded:tt )*
+        }
+        $( $expanded:tt )*
+    ) => {
+        rpc! {
+            $(#[$net_attr])*
+            net: $net_type;
+            { $( $unexpanded )* }
+
+            $( $expanded )*
+
+            $(#[$attr])*
+            rpc $fn_name( $( $arg : $in_ ),* ) -> ();
+        }
+    };
+// Pattern for when the next rpc has an explicit return type and an explicit error type.
+    (
+        $(#[$net_attr:meta])*
+        net: $net_type: ident;
+        {
+            $(#[$attr:meta])*
+            rpc $fn_name:ident( $( $arg:ident : $in_:ty ),* ) -> $out:ty;
+
+            $( $unexpanded:tt )*
+        }
+        $( $expanded:tt )*
+    ) => {
+        rpc! {
+            $(#[$net_attr])*
+            net: $net_type;
+            { $( $unexpanded )* }
+
+            $( $expanded )*
+
+            $(#[$attr])*
+            rpc $fn_name( $( $arg : $in_ ),* ) -> $out;
+        }
+    };
+// Pattern for when all return types have been expanded
+    (
+        $(#[$net_attr:meta])*
+        net: $net_type: ident;
+        { } // none left to expand
+        $(
+            $(#[$attr:meta])*
+            rpc $fn_name:ident ( $( $arg:ident : $in_:ty ),* ) -> $out:ty;
+        )*
+    ) => {
+        #[allow(non_camel_case_types)]
+        #[derive(Debug, Serialize, Deserialize)]
+        enum RpcEnum {
+            $(
+                $fn_name(( $($in_,)* ))
+            ),*
+        }
+
+        pub struct RpcClient(rpc_client!($net_type));
+
+        impl RpcClient {
+            pub fn connect<L: ::std::net::ToSocketAddrs>(addr: L) -> ::std::io::Result<RpcClient> {
+                type Client = rpc_client!($net_type);
+                Client::connect(addr).map(RpcClient)
+            }
+
+            pub fn set_timeout(&mut self, timeout: ::std::time::Duration) {
+                self.0.set_timeout(timeout)
+            }
+
+            $(
+            pub fn $fn_name(&self, $($arg: $in_),*) -> Result<$out, $crate::conetty::Error> {
+                use $crate::conetty::Client;
+                use $crate::conetty::bincode::serde as encode;
+                use $crate::conetty::bincode::SizeLimit::Infinite;
+                use $crate::conetty::Error::{ClientSerialize, ClientDeserialize};
+
+                let mut buf = Vec::with_capacity(1024);
+
+                // serialize the para
+                let para = RpcEnum::$fn_name(($($arg,)*));
+                encode::serialize_into(&mut buf, &para, Infinite)
+                    .map_err(|e| ClientSerialize(e.to_string()))?;
+
+                // call the server
+                let ret = self.0.call_service(&buf)?;
+
+                // deserialized the response
+                encode::deserialize(&ret).map_err(|e| ClientDeserialize(e.to_string()))
+            })*
+        }
+
+        // rpc spec
+        pub trait RpcSpec: Send + Sync + 'static {
+            $(fn $fn_name(&self, $($arg: $in_),*) -> $out;)*
+        }
+
+        // rpc server
+        pub struct RpcServer<T>(pub T);
+
+        impl<T: RpcSpec> ::std::ops::Deref for RpcServer<T> {
+            type Target = T;
+            fn deref(&self) -> &T {
+                &self.0
+            }
+        }
+
+        impl<T: RpcSpec> $crate::conetty::Server for RpcServer<T> {
+            fn service(&self, request: &[u8]) -> Result<Vec<u8>, $crate::conetty::WireError> {
+                use $crate::conetty::bincode::serde as encode;
+                use $crate::conetty::bincode::SizeLimit::Infinite;
+                use $crate::conetty::WireError::{ServerDeserialize, ServerSerialize};
+
+                // deserialize the request
+                let req: RpcEnum = encode::deserialize(request)
+                    .map_err(|e| ServerDeserialize(e.to_string()))?;
+                // dispatch call the service
+                let mut buf = Vec::with_capacity(512);
+                match req {
+                    $(
+                    RpcEnum::$fn_name(($($arg,)*)) => {
+                        let rsp = self.$fn_name($($arg,)*);
+                        // serialize the result
+                        encode::serialize_into(&mut buf, &rsp, Infinite)
+                            .map_err(|e| ServerSerialize(e.to_string()))?;
+                    })*
+                };
+                // send the response
+                Ok(buf)
+            }
+        }
+
+        impl<T: RpcSpec + 'static> RpcServer<T> {
+            pub fn start<L: ::std::net::ToSocketAddrs>(self, addr: L)
+                 -> ::std::io::Result<$crate::conetty::coroutine::JoinHandle<()>> {
+                rpc_server_start!($net_type, self, addr)
+            }
+        }
+    };
+}
